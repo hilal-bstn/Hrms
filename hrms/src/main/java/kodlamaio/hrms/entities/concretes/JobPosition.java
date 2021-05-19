89ef8e7a528a107be5a83ad7f010d38c89faa793
Hilal Baştan <hilalbastan@gmail.com>
2021-05-20 01:32:02 +0300

    Add files via upload

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="job_positions")
+public class JobPosition {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+
+	@Column(name="title")
+	private String Title;
+	
+	public JobPosition() {}
+	
+	public JobPosition(int id, String title) {
+		super();
+		this.id = id;
+		Title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+	
+}
