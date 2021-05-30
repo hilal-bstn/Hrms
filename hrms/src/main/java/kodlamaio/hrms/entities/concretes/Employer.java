f11b09e2fcaaa130a655ca0959fdcf90d7cfb4c3
hilalbastan <hilalbastan@gmail.com>
2021-05-30 16:09:53 +0300

    30-5-2021

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -1,14 +1,23 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 @Entity
 @Table(name="employers")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","employerJobPostings"})
 public class Employer {
 
 	@Id
@@ -16,20 +25,24 @@ public class Employer {
 	@Column(name="id")
 	private int id;
 
-	@Column(name="user_id")
-	private int userId;
+	@OneToOne()
+	@JoinColumn(name="user_id")
+	private User user;
 	
 	@Column(name="company_id")
 	private int companyId;
 	
 	
+	@OneToMany(mappedBy="employer")
+	private List<EmployerJobPosting> employerJobPostings;
+	
 	public Employer() {}
 
 
-	public Employer(int id, int userId, int companyId) {
+	public Employer(int id, User user, int companyId) {
 		super();
 		this.id = id;
-		this.userId = userId;
+		this.user= user;
 		this.companyId = companyId;
 	}
 
@@ -38,29 +51,25 @@ public class Employer {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
-	public int getUserId() {
-		return userId;
-	}
-
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
 
 	public int getCompanyId() {
 		return companyId;
 	}
 
-
 	public void setCompanyId(int companyId) {
 		this.companyId = companyId;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 }
